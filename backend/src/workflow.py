d718d1655c8a0358a11d84672c6d1cee55d3836d
Koustubhmk09 <kulkarnikoustubh21@gmail.com>
2026-05-07 11:03:25 +0530

    initial changes

--- backend/src/workflow.py
+++ backend/src/workflow.py
@@ -3,7 +3,7 @@ from typing import TypedDict, Annotated, List, Union
 from langgraph.graph import StateGraph, END
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 from langchain_groq import ChatGroq
-from src.prompt import classification_prompt, system_prompt, generation_template, contextualize_q_system_prompt
+from .prompt import classification_prompt, system_prompt, generation_template, contextualize_q_system_prompt
 
 class AgentState(TypedDict):
     messages: List[BaseMessage]
